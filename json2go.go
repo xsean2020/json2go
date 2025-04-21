2436864f942904bf43d347d27a603cbc0fe40df3
Yangzhiqin <yangzhiqin@neuronads.com>
2025-04-21 15:07:44 +0800

    fix bug

--- json2go.go
+++ json2go.go
@@ -25,6 +25,18 @@ type StructType struct {
 	Fields map[string]*StructType
 }
 
+func isRefType(t string) bool {
+	switch t {
+	case TypeMap, TypeArray:
+		return true
+	}
+	return false
+}
+
+func (s *StructType) IsNumber() bool {
+	return s.Type == TypeInt || s.Type == TypeFloat
+}
+
 func (s *StructType) GoType(isAnonymous bool) string {
 	if isAnonymous && s.Type == TypeMap {
 		// 如果是匿名结构体，返回结构体定义的内容
@@ -41,7 +53,7 @@ func (s *StructType) GoType(isAnonymous bool) string {
 			if v.Type == TypeMap {
 				t = "*"
 			}
-			sb.WriteString(fmt.Sprintf("    %s %s%s `json:\"%s\"`\n", k, t, v.GoType(isAnonymous), strings.ToLower(k)))
+			sb.WriteString(fmt.Sprintf("    %s %s%s `json:\"%s\"`\n", k, t, v.GoType(isAnonymous), v.Tag))
 		}
 		sb.WriteString("}")
 		return sb.String()
@@ -63,24 +75,23 @@ func (s *StructType) GenGoStructs(isAnonymous bool) string {
 	}
 
 	var structs []string
-	visited := make(map[string]bool)
-
 	var collect func(st *StructType)
 	collect = func(st *StructType) {
-		if st.Type != TypeMap {
+		if !isRefType(st.Type) {
 			return
 		}
+
 		key := st.Parent + st.Name
-		if visited[key] {
-			return
-		}
-		visited[key] = true
 		if !isAnonymous {
+			if st.Type == TypeArray {
+				collect(st.Fields[ArrayKey])
+				return
+			}
+
 			for _, f := range st.Fields {
 				collect(f)
 			}
 		}
-
 		var sb strings.Builder
 		sb.WriteString(fmt.Sprintf("type %s struct {\n", key))
 
@@ -92,13 +103,12 @@ func (s *StructType) GenGoStructs(isAnonymous bool) string {
 
 		for _, k := range keys {
 			v := st.Fields[k]
-
 			var tmp = ""
 			if v.Type == TypeMap {
 				tmp = "*"
 			}
 
-			sb.WriteString(fmt.Sprintf("    %s %s%s `json:\"%s\"`\n", k, tmp, v.GoType(isAnonymous), strings.ToLower(k)))
+			sb.WriteString(fmt.Sprintf("    %s %s%s `json:\"%s\"`\n", k, tmp, v.GoType(isAnonymous), v.Tag))
 		}
 		sb.WriteString("}\n")
 		structs = append(structs, sb.String())
@@ -109,8 +119,15 @@ func (s *StructType) GenGoStructs(isAnonymous bool) string {
 
 // mergeStruct 合并两个 map 类型结构体的字段，如果字段类型不同则跳过合并
 func mergeStruct(dest, src *StructType) {
+
 	if dest.Type != src.Type {
-		panic(fmt.Errorf("type error at %s", dest.Parent+dest.Name))
+		if dest.IsNumber() && src.IsNumber() {
+			if src.Type == TypeFloat {
+				dest.Type = src.Type
+			}
+			return
+		}
+		panic(fmt.Errorf("type error at %s %s <- %s", dest.Parent+dest.Name, dest.Type, src.Type))
 	}
 
 	if dest.Type != TypeMap || src.Type != TypeMap {
@@ -120,7 +137,7 @@ func mergeStruct(dest, src *StructType) {
 	for k, v := range src.Fields {
 		if existing, ok := dest.Fields[k]; ok {
 			// 只合并同类型字段
-			if existing.Type == v.Type {
+			if existing.Type == v.Type || (existing.IsNumber() && v.IsNumber()) {
 				mergeStruct(existing, v)
 			} else {
 				// 处理不同类型字段的警告
@@ -149,7 +166,8 @@ func DetectType(val any, name string, parent string) *StructType {
 // detectType 根据输入的值推断字段的类型，并生成相应的 StructType
 func detectType(val any, name string, parent string) *StructType {
 	st := &StructType{
-		Name:   name,
+		Name:   toExported(name),
+		Tag:    name,
 		Parent: parent,
 		Fields: make(map[string]*StructType),
 	}
@@ -171,22 +189,23 @@ func detectType(val any, name string, parent string) *StructType {
 		st.Type = TypeArray
 		if len(v) > 0 {
 			// 推断数组元素类型
-			elem := detectType(v[0], "", parent+name)
+			elem := detectType(v[0], name, parent+name)
 			st.Fields[ArrayKey] = elem
 			for _, item := range v[1:] {
-				elem := detectType(item, "", parent+name)
+				elem := detectType(item, name, parent+name)
 				mergeStruct(st.Fields[ArrayKey], elem)
 			}
 		} else {
 			st.Fields[ArrayKey] = &StructType{Type: "interface{}"}
 		}
+
 	case map[string]any:
 		st.Type = "map"
 		for key, value := range v {
-			goField := toExported(key)
-			field := detectType(value, goField, parent+name)
-			st.Fields[goField] = field
+			field := detectType(value, key, parent+name)
+			st.Fields[field.Name] = field
 		}
+
 	default:
 		st.Type = "interface{}"
 	}
